85682f4cb72f6bb3127895795d3c0467ee9cb76d
Michiel <Michielnooij@icloud.com>
2026-05-18 20:39:38 +0200

    Initial Commit

---
+++ YouAreGreat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreGreat
+//
+//  Created by Michiel Nooij on 18/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreGreat/YouAreGreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreGreatApp.swift
+//  YouAreGreat
+//
+//  Created by Michiel Nooij on 18/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct YouAreGreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
